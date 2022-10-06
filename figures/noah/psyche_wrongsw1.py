e25af25abe26282a63d3162611083580bc4d95a1
Jonathan Yong <yongrenjie@gmail.com>
2022-10-06 23:09:07 +0100

    Submission

--- figures/noah/psyche_wrongsw1.py
+++ figures/noah/psyche_wrongsw1.py
@@ -5,12 +5,28 @@ apt.thesis()
 
 p = apt.nmrd() / '211123-7c-psyche-sapphire'
 
-dss = pg.read(p, [101004, 102004, 11004, 13004])
+dss = pg.read(p, [101004, 102004, 11004, 13004, 21004, 23004])
 
-fig, axs = pg.subplots2d(2, 2, figsize=(6.5, 4), sharey=True)
-for ds, ax in zip(dss, axs.flat):
-    ds.stage(ax=ax, color='black', bounds=(0.4, 3.4))
+labels = [
+    'no SAPPHIRE, 50 Hz, misset chunk size',
+    'SAPPHIRE, 50 Hz, misset chunk size',
+    'no SAPPHIRE, 50 Hz',
+    'SAPPHIRE, 50 Hz',
+    'no SAPPHIRE, 25 Hz',
+    'SAPPHIRE, 25 Hz',
+]
+
+fig, axs = pg.subplots2d(3, 2, figsize=(6.5, 6))
+for ds, ax, label in zip(dss, axs.flat, labels):
+    ds.stage(ax=ax, color='black', bounds=(0.4, 3.4), linewidth=0.8)
+    ax.text(x=0.10, y=0.975, s=label, ha='left', va='top',
+            transform=ax.transAxes)
     pg.mkplot()
+
+ymin = min(ax.get_ylim()[0] for ax in axs.flat)
+ymax = max(ax.get_ylim()[1] for ax in axs.flat)
+for ax in axs.flat:
+    ax.set_ylim((ymin, ymax * 1.2))
 
 apt.label_axes_def(axs)
 
